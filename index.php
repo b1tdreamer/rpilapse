5cf87a1e7b0a9efb2a147cb595f33e68421273c3
Fer <b1tdreamer@tabahack.tk>
2014-08-17 13:38:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Rpi Timelapse</title>
+  </head>
+  <body>
+    <h1></h1>
+<?
+//Lee las imágenes del directorio y las anima en javascript
+while(){
+?>
+    <img src="<?=$file['src']?>" />
+<?
+}
+?>
+  </body>
+</html>
